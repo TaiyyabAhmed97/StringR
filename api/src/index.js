88176c720d6fd0e6076371ded2adfdd0172223e7
Taiyyab <hafiztaiyyab11@gmail.com>
2020-06-04 13:50:40 -0500

    added es6 to node backend

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+import redis from 'redis'
+
+let client = redis.createClient()
+
+
+client.on('connect', () => {
+    console.log(' Redis client connected')
+})
+
+client.on('error', (err) => {
+    console.log(' Redis client  not connected')
+})
