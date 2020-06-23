720113f11a3a42b4c1bb7e9400c8a3b9b2ff206a
Taiyyab <hafiztaiyyab11@gmail.com>
2020-06-23 14:23:58 -0500

    added due date field to restring form

--- frontend/src/components/restring-form/restring-form.js
+++ frontend/src/components/restring-form/restring-form.js
@@ -1,23 +1,40 @@
 import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "../restring-form/restring-form.css";
 
 export default class RestringForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { name: "", phoneNumber: "" };
+    this.state = { name: "", phoneNumber: "", dueDate: new Date() };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
   }
 
   handleSubmit(e) {
-    console.log(this.state);
+    this.setState(
+      {
+        currentDate: new Date(),
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+
     e.preventDefault();
   }
   handleChange(e) {
     const name = e.target.name;
     this.setState({
       [name]: e.target.value,
+    });
+  }
+  handleDateChange(date) {
+    this.setState({
+      dueDate: date,
     });
   }
 
@@ -48,6 +65,17 @@ export default class RestringForm extends Component {
                 placeholder="e.g. 773-712-8894"
                 onChange={this.handleChange}
               />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Due Date</label>
+            <div className="control">
+              <div className="datePicker">
+                <DatePicker
+                  selected={this.state.dueDate}
+                  onChange={this.handleDateChange}
+                />
+              </div>
             </div>
           </div>
 
