cc22b20ae107fa4ed7818f5dfbe1ccb2e5750ae0
Taiyyab <hafiztaiyyab11@gmail.com>
2020-07-01 15:15:04 -0500

    added base string jobs cards

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
+import "../dashboard/dashboard.css";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -8,21 +9,39 @@ class Dashboard extends Component {
     this.state = {
       stringjobs: [],
     };
+    this.stringJobCards = this.stringJobCards.bind(this);
   }
   componentDidMount() {
-    axios.get("http://localhost:8000/api/stringjob/notdone").then((res) => {
-      console.log(res);
+    axios.get("http://localhost:8000/api/stringjob").then((res) => {
       this.setState({
         stringjobs: res.data.data,
       });
     });
   }
+
+  rstFields() {}
+  stringJobCards() {
+    const sJobs = this.state.stringjobs.map((obj, idx) => {
+      return (
+        <div key={idx}>
+          <div className="box stringJobCard">Hello World</div>
+        </div>
+      );
+    });
+    return sJobs;
+  }
+
   render() {
-    return (
-      <div>
-        <pre>{JSON.stringify(this.state, null, 2)} </pre>
-      </div>
-    );
+    if (this.state.stringjobs.length == 0) {
+      return null;
+    } else {
+      return (
+        <div>
+          {this.stringJobCards()}
+          <pre>{JSON.stringify(this.state, null, 2)} </pre>
+        </div>
+      );
+    }
   }
 }
 export default withRouter(Dashboard);
