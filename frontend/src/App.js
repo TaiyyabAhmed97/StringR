aef0640b3165402751be3fa0b157323ec19dd2a1
Taiyyab <hafiztaiyyab11@gmail.com>
2020-07-01 20:21:59 -0500

    main theme changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,22 +9,53 @@ import Dashboard from "./components/dashboard/dashboard";
 export default function App() {
   return (
     <>
-      <div className="root">
-        <h1 className="title">Strings Attached Restringing Application</h1>
-        <Router>
-          <Switch>
-            <Route path="/restring">
-              <RestringForm></RestringForm>
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard></Dashboard>
-            </Route>
-            <Route path="/">
-              <NewUserForm></NewUserForm>
-            </Route>
-          </Switch>
-        </Router>
+      <div>
+        <section className="hero is-primary">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                Strings Attached Restringing Application
+              </h1>
+            </div>
+          </div>
+        </section>
       </div>
+      <section className="section">
+        <div className="container">
+          <Router>
+            <Switch>
+              <Route path="/restring">
+                <RestringForm></RestringForm>
+              </Route>
+              <Route path="/dashboard">
+                <Dashboard></Dashboard>
+              </Route>
+              <Route path="/">
+                <NewUserForm></NewUserForm>
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </section>
+      <section class="section">
+        <footer class="footer">
+          <div class="container">
+            <div class="content has-text-centered">
+              <p>
+                <strong>Bulma</strong> by{" "}
+                <a href="https://jgthms.com">Jeremy Thomas</a>. The source code
+                is licensed
+                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>
+                . The website content is licensed{" "}
+                <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+                  CC BY NC SA 4.0
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </footer>
+      </section>
     </>
   );
 }
