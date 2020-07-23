3539822aac408d320f4473a0632f94e9fa2c07bd
Taiyyab <hafiztaiyyab11@gmail.com>
2020-07-23 08:45:56 -0500

    pushed build folder

---
+++ frontend/build/precache-manifest.bc267e09e2724c9b9ebe1af02ef72d19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c3e2d8b410aeff523d33a789196251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bc89e3ef24dc69d0633",
+    "url": "/static/css/2.65e3e712.chunk.css"
+  },
+  {
+    "revision": "8ffef7038ff9de2060aa",
+    "url": "/static/css/main.e95a5f18.chunk.css"
+  },
+  {
+    "revision": "5bc89e3ef24dc69d0633",
+    "url": "/static/js/2.2ae71ae8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.2ae71ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ffef7038ff9de2060aa",
+    "url": "/static/js/main.e7b89fae.chunk.js"
+  },
+  {
+    "revision": "04cbad211563d7ed4ff7",
+    "url": "/static/js/runtime-main.d2c417ed.js"
+  }
+]);
